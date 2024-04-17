1006444421dc68e041038deb7641597b3222f81b
joodi.a <joodi.a@C200000-8>
2024-04-17 13:12:49 +0330

    Add project files.

---
+++ BankoOfDotNetAPI/Data/BankContext.cs
@@ -0,0 +1,14 @@
+﻿using BankoOfDotNetAPI.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace BankoOfDotNetAPI.Data
+{
+    public class BankContext : DbContext
+    {
+        public BankContext(DbContextOptions<BankContext> options) : base(options)
+        {
+
+        }
+        public DbSet<Customer> Customers { get; set; }
+    }
+}
---
+++ BankoOfDotNetAPI/Models/Customer.cs
@@ -0,0 +1,11 @@
+﻿using BankoOfDotNetAPI.Data;
+using Microsoft.EntityFrameworkCore;
+namespace BankoOfDotNetAPI.Models
+{
+    public class Customer
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+    }
+}
